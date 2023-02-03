6c31df70b220ac8c0942653f6fb95c3847c98045
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-02-03 15:28:34 +0100

    fixing hcloud-rffmpeg

--- hcloud-rffmpeg.py
+++ hcloud-rffmpeg.py
@@ -36,28 +36,6 @@ def fail(msg):
 def setup():
     config = dict()
 
-
-    STATE_DIR = os.getenv("STATE_DIR")
-    if STATE_DIR == None:
-        STATE_DIR = "/config/rffmpeg"
-    config["state_dir"] = STATE_DIR
-
-    LOG_FILE = os.getenv("LOG_FILE")
-    if LOG_FILE == None:
-        LOG_FILE = STATE_DIR + "/hcloud-rffmpeg.log"
-    config["log_file"] = LOG_FILE
-
-    DB_PATH = os.getenv("DB_PATH")
-    if DB_PATH == None:
-        DB_PATH = STATE_DIR + "/rffmpeg.db"
-    config["db_path"] = DB_PATH
-
-    ID_RSA_PUB = os.getenv("ID_RSA_PUB")
-    if ID_RSA_PUB == None:
-        ID_RSA_PUB = STATE_DIR + "/.ssh/id_rsa.pub"
-    config["id_rsa_pub"] = ID_RSA_PUB
-
-
     HCLOUD_API_TOKEN = os.getenv("HCLOUD_API_TOKEN")
     if HCLOUD_API_TOKEN == None:
         fail("HCLOUD_API_TOKEN env isn't set")
@@ -65,87 +43,42 @@ def setup():
     JELLYFIN_LAN_ONLY_IP = os.getenv("JELLYFIN_LAN_ONLY_IP")
     if JELLYFIN_LAN_ONLY_IP == None:
         fail("JELLYFIN_LAN_ONLY_IP env isn't set")
+    
+    MEDIA_USERNAME = os.getenv("MEDIA_USERNAME", "")
+    MEDIA_PASSWORD = os.getenv("MEDIA_PASSWORD", "")
+    config["cloud_config"] = os.getenv("CLOUD_CONFIG", "#cloud-config\nruncmd:\n- systemctl disable --now ssh.service\n- echo 'JELLYFIN_LAN_ONLY_IP=%s' | tee -a /root/.env\n- echo 'MEDIA_USERNAME=%s' | tee -a /root/.env\n- echo 'MEDIA_PASSWORD=%s' | tee -a /root/.env\n- fallocate -l 4G /swapfile\n- chmod 600 /swapfile\n- mkswap /swapfile\n- swapon /swapfile\n- echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab\n- wget https://raw.githubusercontent.com/aleksasiriski/rffmpeg-worker/main/docker-compose.example.yml -O /root/docker-compose.yml\n- cd /root && docker compose pull && docker compose up -d\n"%(JELLYFIN_LAN_ONLY_IP, MEDIA_USERNAME, MEDIA_PASSWORD))
 
-    MEDIA_USERNAME = os.getenv("MEDIA_USERNAME")
-    if MEDIA_USERNAME == None:
-        fail("MEDIA_USERNAME env isn't set")
-
-    MEDIA_PASSWORD = os.getenv("MEDIA_PASSWORD")
-    if MEDIA_PASSWORD == None:
-        fail("MEDIA_PASSWORD env isn't set")
-
+    config["state_dir"] = os.getenv("STATE_DIR", "/config/rffmpeg")
+    config["log_file"] = os.getenv("LOG_FILE", config["state_dir"] + "/hcloud-rffmpeg.log")
+    config["db_path"] = os.getenv("DB_PATH", config["state_dir"] + "/rffmpeg.db")
+    config["ssh_key"] = os.getenv("SSH_KEY", config["state_dir"] + "/.ssh/id_rsa.pub")
 
     config["client"] = Client(token=HCLOUD_API_TOKEN)
+    config["server_type"] = os.getenv("SERVER_TYPE", "cx21")
+    config["image"] = Image(name=os.getenv("IMAGE_TYPE", "docker-ce"))
+    config["networks"] = config["client"].networks.get_all(name=os.getenv("NETWORK_NAME", "rffmpeg-workers"))
+    config["firewalls"] = config["client"].firewalls.get_all(name=os.getenv("FIREWALL_NAME", "rffmpeg-workers"))
+    config["placement_group"] = config["client"].placement_groups.get_by_name(name=os.getenv("PLACEMENT_GROUP_NAME", "rffmpeg-workers"))
+    config["location"] = Location(name=os.getenv("LOCATION_NAME", "nbg1"))
+    config["jobs_per_worker"] = int(os.getenv("JOBS_PER_WORKER", "2"))
+    config["recently_made_worker_bool"] = False
 
-    SERVER_TYPE = os.getenv("SERVER_TYPE")
-    if SERVER_TYPE == None:
-        SERVER_TYPE = "cx21"
-    config["server_type"] = ServerType(name=SERVER_TYPE)
-
-    IMAGE_TYPE = os.getenv("IMAGE_TYPE")
-    if IMAGE_TYPE == None:
-        IMAGE_TYPE = "docker-ce"
-    config["image"] = Image(name=IMAGE_TYPE)
-
-
-    SSH_KEY_NAME = os.getenv("SSH_KEY_NAME")
-    if SSH_KEY_NAME == None:
-        SSH_KEY_NAME = "root@jellyfin"
-
+    SSH_KEY_NAME = os.getenv("SSH_KEY_NAME", "root@jellyfin")
     ssh_key = config["client"].ssh_keys.get_by_name(name=SSH_KEY_NAME)
     try:
         config["client"].ssh_keys.delete(ssh_key)
         log.debug("Found key and removed it.")
     except:
         log.debug("No key found to remove.")
-
     public_key = ""
-    with open(config["id_rsa_pub"], 'r') as id_rsa_pub_file:
-        public_key = id_rsa_pub_file.readline()
-        id_rsa_pub_file.close()
-
+    with open(config["ssh_key"], 'r') as ssh_key_file:
+        public_key = ssh_key_file.readline()
+        ssh_key_file.close()
     config["client"].ssh_keys.create(
         name=SSH_KEY_NAME,
         public_key=public_key
     )
-
     config["ssh_keys"] = config["client"].ssh_keys.get_all(name=SSH_KEY_NAME)
-
-
-    NETWORK_NAME = os.getenv("NETWORK_NAME")
-    if NETWORK_NAME == None:
-        NETWORK_NAME = "rffmpeg"
-    config["networks"] = config["client"].networks.get_all(name=NETWORK_NAME)
-
-    FIREWALL_NAME = os.getenv("FIREWALL_NAME")
-    if FIREWALL_NAME == None:
-        FIREWALL_NAME = "rffmpeg"
-    config["firewalls"] = config["client"].firewalls.get_all(name=FIREWALL_NAME)
-
-    PLACEMENT_GROUP_NAME = os.getenv("PLACEMENT_GROUP_NAME")
-    if PLACEMENT_GROUP_NAME == None:
-        PLACEMENT_GROUP_NAME = "rffmpeg"
-    config["placement_group"] = config["client"].placement_groups.get_by_name(name=PLACEMENT_GROUP_NAME)
-
-    LOCATION_NAME = os.getenv("LOCATION_NAME")
-    if LOCATION_NAME == None:
-        LOCATION_NAME = "nbg1"
-    config["location"] = Location(name=LOCATION_NAME)
-
-
-    CLOUD_CONFIG = os.getenv("CLOUD_CONFIG")
-    if CLOUD_CONFIG == None:
-        CLOUD_CONFIG = "#cloud-config\nruncmd:\n- systemctl disable --now ssh.service\n- echo 'JELLYFIN_LAN_ONLY_IP=%s' | tee -a /root/.env\n- echo 'MEDIA_USERNAME=%s' | tee -a /root/.env\n- echo 'MEDIA_PASSWORD=%s' | tee -a /root/.env\n- fallocate -l 4G /swapfile\n- chmod 600 /swapfile\n- mkswap /swapfile\n- swapon /swapfile\n- echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab\n- wget https://raw.githubusercontent.com/aleksasiriski/rffmpeg-worker/main/docker-compose.example.yml -O /root/docker-compose.yml\n- cd /root && docker compose pull && docker compose up -d\n"%(JELLYFIN_LAN_ONLY_IP, MEDIA_USERNAME, MEDIA_PASSWORD)
-    config["cloud_config"] = CLOUD_CONFIG
-
-
-    JOBS_PER_WORKER = os.getenv("JOBS_PER_WORKER")
-    if JOBS_PER_WORKER == None:
-        JOBS_PER_WORKER = 2
-    config["jobs_per_worker"] = int(JOBS_PER_WORKER)
-
-    config["recently_made_node_bool"] = False
-
 
     return config
 
@@ -159,52 +92,26 @@ def dbconn(config):
     conn.close()
 
 
-async def recently_made_node_timer(config, delay):
+async def recently_made_worker_timer(config, delay):
     await asyncio.sleep(delay)
-    config["recently_made_node_bool"] = False
-    log.debug("Timer finished, able to make nodes again!")
+    config["recently_made_worker_bool"] = False
+    log.debug("Timer finished, able to make workers again!")
 
-async def recently_made_node(config):
-    if config["recently_made_node_bool"]:
+async def recently_made_worker(config):
+    if config["recently_made_worker_bool"]:
         return True
     else:
-        config["recently_made_node_bool"] = True
-        asyncio.create_task(recently_made_node_timer(config, 180))
+        config["recently_made_worker_bool"] = True
+        asyncio.create_task(recently_made_worker_timer(config, 180))
         return False
-
-async def next_node_name(config):
-    log.debug("Generating name for the next node.")
-        
-    with dbconn(config) as cur:
-        hosts = cur.execute("SELECT * FROM hosts").fetchall()
-
-    name = "rffmpegnode"
-    next_number = 1
-
-    if len(hosts) < 1:
-        log.debug("No nodes found.")
-    else:
-        log.debug("Using largest node number + 1.")
-        for host in hosts:
-            hid, hostname, weight, servername = host
-            node_number = servername.split(name, 1)[1]
-            if next_number <= node_number:
-                next_number = node_number + 1
-    
-    new_name = name + str(next_number)
-    log.debug("New name is " + new_name)
-
-    return new_name
 
 async def create_server(config):
     log.info("Creating a server!")
 
-    if not await recently_made_node(config):
+    if not await recently_made_worker(config):
         log.debug("No recently made servers!")
 
-        servername = await next_node_name(config)
         response = config["client"].servers.create(
-            name=servername,
             server_type=config["server_type"],
             image=config["image"],
             ssh_keys=config["ssh_keys"],
@@ -215,31 +122,32 @@ async def create_server(config):
             user_data=config["cloud_config"]
         )
 
+        await asyncio.sleep(120)
         if response.action.status == "error":
             log.error("Error occured while creating the server in HCloud!")
         else:
-            await asyncio.sleep(120)
             log.debug("Successfully created a server in HCloud!")
 
-            server_ip = config["client"].servers.get_by_name(name=servername).private_net[0].ip
+            server_name = response.model.name
+            server_ip = config["client"].servers.get_by_name(name=server_name).private_net[0].ip
 
             with dbconn(config) as cur:
                 cur.execute(
                     "INSERT INTO hosts (hostname, weight, servername) VALUES (?, ?, ?)",
-                    (server_ip, 1, servername),
+                    (server_ip, 1, server_name),
                 )
 
-            log.info("Added %s with IP %s to database!"%(servername,server_ip))
-            asyncio.create_task(check_unused_node(config, servername))
+            log.info("Added %s with IP %s to database!"%(server_name,server_ip))
+            asyncio.create_task(check_unused_worker(config, server_name))
 
     else:
         log.debug("Recently made a server!")
 
-async def remove_server(config, servername):
-    server = config["client"].servers.get_by_name(name=servername)
+async def remove_server(config, server_name):
+    server = config["client"].servers.get_by_name(name=server_name)
 
     with dbconn(config) as cur:
-        cur.execute("DELETE FROM hosts WHERE servername = ?", (servername,))
+        cur.execute("DELETE FROM hosts WHERE server_name = ?", (server_name,))
 
     try:
         config["client"].servers.delete(server)
@@ -247,8 +155,8 @@ async def remove_server(config, servername):
     except:
         log.debug("No server found to remove.")
 
-async def check_unused_node(config, servername):
-    log.debug("Started checking if %s is unused, firstly sleeping for 50 minutes"%(servername))
+async def check_unused_worker(config, server_name):
+    log.debug("Started checking if %s is unused, firstly sleeping for 50 minutes"%(server_name))
 
     delay_hour = 3000
     delay_ending_hour = 240
@@ -256,7 +164,7 @@ async def check_unused_node(config, servername):
 
     with dbconn(config) as cur:
         host = cur.execute(
-            "SELECT * FROM hosts WHERE servername = ?", (servername,)
+            "SELECT * FROM hosts WHERE server_name = ?", (server_name,)
         ).fetchone()
     host_id = host[0]
 
@@ -264,7 +172,7 @@ async def check_unused_node(config, servername):
     while not removed:
         # how many times to check and sleep for 4 minutes after initial 50 minutes
         for counter in range(2):
-            log.debug("Checking if node %s is unused"%(servername))
+            log.debug("Checking if worker %s is unused"%(server_name))
 
             with dbconn(config) as cur:
                 current_state = cur.execute(
@@ -277,12 +185,12 @@ async def check_unused_node(config, servername):
                 current_state = current_state[3]
 
             if current_state != "active":
-                log.info("Node %s marked as inactive and is being removed."%(servername))
-                await remove_server(config, servername)
+                log.info("Worker %s marked as inactive and is being removed."%(server_name))
+                await remove_server(config, server_name)
                 removed = True
                 break
             else:
-                log.debug("Node %s marked as active, sleeping."%(servername))
+                log.debug("Worker %s marked as active, sleeping."%(server_name))
                 await asyncio.sleep(delay_ending_hour)
 
         if not removed:
@@ -304,20 +212,20 @@ async def remove_all_processes(config):
             with dbconn(config) as cur:
                 cur.execute("DELETE FROM processes WHERE id = ?", (pid,))
 
-async def remove_all_nodes(config):
-    log.info("Removing all nodes from database and HCloud.")
+async def remove_all_workers(config):
+    log.info("Removing all workers from database and HCloud.")
         
     with dbconn(config) as cur:
         hosts = cur.execute("SELECT * FROM hosts").fetchall()
 
     if len(hosts) < 1:
-        log.debug("No nodes found.")
+        log.debug("No workers found.")
     else:
-        log.debug("Removing all nodes.")
+        log.debug("Removing all workers.")
         for host in hosts:
-            hid, hostname, weight, servername = host
-            log.debug("Removing node %s."%(servername))
-            await remove_server(config, servername)
+            hid, hostname, weight, server_name = host
+            log.debug("Removing worker %s."%(server_name))
+            await remove_server(config, server_name)
 
 
 async def check_processes_and_rescale(config):
@@ -329,7 +237,7 @@ async def check_processes_and_rescale(config):
             processes = cur.execute("SELECT * FROM processes").fetchall()
 
         if len(hosts) < 1:
-            log.debug("No nodes found. Checking if there are any transcodes on fallback.")
+            log.debug("No workers found. Checking if there are any transcodes on fallback.")
             transcodes = 0
             for process in processes:
                 pid, host_id, process_id, cmd = process
@@ -340,11 +248,11 @@ async def check_processes_and_rescale(config):
                 log.info("Found transcodes on fallback!")
                 asyncio.create_task(create_server(config))
         else:
-            log.debug("Nodes found. Checking if there are any nodes with room.")
-            nodes_with_room = 0
+            log.debug("Workers found. Checking if there are any workers with room.")
+            workers_with_room = 0
             for host in hosts:
                 transcodes = 0
-                hid, hostname, weight, servername = host
+                hid, hostname, weight, server_name = host
 
                 for process in processes:
                     pid, host_id, process_id, cmd = process
@@ -352,13 +260,13 @@ async def check_processes_and_rescale(config):
                         transcodes += 1
 
                 if transcodes < config["jobs_per_worker"]:
-                    nodes_with_room += 1
+                    workers_with_room += 1
 
-            if nodes_with_room == 0:
-                log.debug("No nodes with room found.")
+            if workers_with_room == 0:
+                log.debug("No workers with room found.")
                 asyncio.create_task(create_server(config))
             else:
-                log.debug("Nodes with room found.")
+                log.debug("Workers with room found.")
 
         log.debug("Sleeping for 5 minutes until next check.")
         await asyncio.sleep(300)
@@ -374,7 +282,7 @@ async def main():
         fail("Failed to find database %s - did you forget to run 'rffmpeg init'?"%(config["db_path"]))
 
     await remove_all_processes(config)
-    await remove_all_nodes(config)
+    await remove_all_workers(config)
 
     await check_processes_and_rescale(config)
 
